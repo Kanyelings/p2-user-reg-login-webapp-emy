615cbd433764725356c23ed0019c7959205916df
Njong392 <njongemy@gmail.com>
2021-07-19 21:47:10 +0100

    Registration page

---
+++ Regitration.php
@@ -0,0 +1,35 @@
+
+<!Doctype html>
+<html>
+  <head>
+    <title> Registration page</title>
+  </head>
+    <body>
+      <form action="" method="POST">
+          <label for="firstname">First name</label>
+          <input type="text" id="firstname" name="firstname" placeholder="Enter first name" required>
+
+          <label for="lastname">Last name</label>
+          <input type="text" id="lastname" name="lastname" placeholder="Enter last name" required>
+
+          <br><br>
+
+          <label for="username">Username</label>
+          <input type="text" id="username" name="username" placeholder="Enter a username" required>
+
+
+          <label for="email">Email</label>
+          <input type="email" id="email" name="email" placeholder="Enter your email" required>
+
+          <label for="sex">Sex</label>
+          <input type="radio" id="sex" name="sex" value="Male">
+          <input type="radio" id="sex" name="sex" value="Female">
+
+
+      </form>
+
+
+    </body>
+</html>
+
+
