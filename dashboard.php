8356727cf28a6a82a0486311e4417bc4cb295466
Njong392 <njongemy@gmail.com>
2021-07-26 21:26:40 +0100

    Dashboard edit

--- dashboard.php
+++ dashboard.php
@@ -1,3 +1,18 @@
-<?php
+<!Doctype html>
+<html lang="en">
 
-echo 'Hey there, you successfully logged in';
+<head>
+    <title>dashboard</title>
+</head>
+  <body>
+
+  <h1>WELCOME!</h1>
+
+  <p>We are glad you could join us. Learner has been helping thousands of people get
+      the skills they want in whatever field they chose to pursue. And guess what? It's free </p>
+
+
+  </body>
+
+
+</html>
