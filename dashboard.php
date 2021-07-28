ea291ffe0ce56f670a5274214bfcdc7679819841
Njong392 <njongemy@gmail.com>
2021-07-28 22:20:44 +0100

    New dashboard

--- dashboard.php
+++ dashboard.php
@@ -1,31 +1,60 @@
-<!Doctype html>
-<html lang="en">
+!Doctype html>
+	<head>
+		<meta charset="utf-8">
+		<title>Dashboard</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
 
-<head>
-    <title>dashboard</title>
-    <link rel="stylesheet" href="CSS/style.css">
-</head>
-  <body class="body">
-   <h1 class="welcome" style="float: top">Welcome!</h1>
+	<body class="body">
+		
+		<h1 class="intro" style="float: left;">Welcome!</h1>
+		<br><br>
 
-   <br><br>
+		
+			<br><br>
+			<h3>Fill in the below information to get started</h3>
+			<form action="" method="POST">
+				<fieldset id="box1">
+					<legend class="age">Age</legend>
+					<label class="label1" for="age">14-18</label>
+					<input type="radio" name="age" id="age">
+					<label class="label2" for="age">19-23</label>
+					<input type="radio" name="age" id="age">
+					<label class="label3" for="age">24+</label>
+					<input type="radio" name="age" id="age">
 
-   <div class="choices">
-       <div class="btn1">
-       <button id="btn1" type="button">Take a course</button>
-       </div>
-       <br><br>
-       <div class="btn2">
-       <button id="btn2" type="button">Check out our books</button>
-       </div>
-       <br><br>
-       <div class="btn3">
-       <button id="btn3" type="button">Give us a review</button>
-       </div>
+				</fieldset>
 
-   </div>
+				<br><br>
+
+       <fieldset id="box2">
+	       <legend class="course">Course related<legend>
+                <p class="par">What courses would you like to pursue?</p>
+		<label class="check1" for="check1">Machine Learning</label>
+		<input type="checkbox" name="check" id="check1">
+		<br>
+		<label class="check2" for="check2">Data Science with python</label>
+		<input type="checkbox" name="check" id="check2">
+		<br>
+		<label class="check3" for="check3">Web Development</label>
+		<input type="checkbox" name="check" id="check3">
+		<br>
+		<label class="check4" for="check4">Introduction to Python</label>
+		<input type="checkbox" name="check" id="check4">
+
+	</fieldset>
 
 
 
-  </body>
+
+
+			</form>
+			
+
+		
+	</body>
+
+
+</html>
+
 
