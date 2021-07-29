051f19eacc41e354a4fb19e9324594dc6ebd2218
Njong392 <njongemy@gmail.com>
2021-07-29 17:53:51 +0100

    Edited

--- dashboard.php
+++ dashboard.php
@@ -1,8 +1,9 @@
-!Doctype html>
+<!Doctype html>
 	<head>
 		<meta charset="utf-8">
+         <meta name="viewport" content="width=device-width, initial-scale=1"
 		<title>Dashboard</title>
-		<link rel="stylesheet" type="text/css" href="style.css">
+		<link rel="stylesheet" type="text/css" href="dashboardstyle.css">
 	</head>
 
 	<body class="body">
@@ -27,25 +28,28 @@
 
 				<br><br>
 
-       <fieldset id="box2">
-	       <legend class="course">Course related<legend>
-                <p class="par">What courses would you like to pursue?</p>
-		<label class="check1" for="check1">Machine Learning</label>
-		<input type="checkbox" name="check" id="check1">
-		<br>
-		<label class="check2" for="check2">Data Science with python</label>
-		<input type="checkbox" name="check" id="check2">
-		<br>
-		<label class="check3" for="check3">Web Development</label>
-		<input type="checkbox" name="check" id="check3">
-		<br>
-		<label class="check4" for="check4">Introduction to Python</label>
-		<input type="checkbox" name="check" id="check4">
+				<fieldset id="box2">
+					<legend class="course">Course related</legend>
+					<p class="par">What courses would you like to pursue?</p>
+					<label class="check1" for="check1">Machine Learning</label>
+					<input type="checkbox" name="check" id="check1">
+					<br>
+					<label class="check2" for="check2">Data Science with python</label>
+					<input type="checkbox" name="check" id="check2">
+					<br>
+					<label class="check3" for="check3">Web Development</label>
+					<input type="checkbox" name="check" id="check3">
+					<br>
+					<label class="check4" for="check4">Introduction to Python</label>
+					<input type="checkbox" name="check" id="check4">
 
-	</fieldset>
+				</fieldset>
 
 
 
+
+
+				
 
 
 			</form>
